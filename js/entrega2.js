cb8af9bd5e8b98c6f9acf5b2d43f4ba83cedc444
Ivan Adi <ivo.adi_18@hotmail.com.ar>
2022-02-28 18:32:08 -0300

    entrega2

--- js/entrega2.js
+++ js/entrega2.js
@@ -6,5 +6,5 @@ promedio = (parcial+parcial2)/2;
 if(promedio >= 7){
     document.write ("Su promedio es de "+promedio+" . ESTA APROBADO, Nos vemos en el final en 3 semanas,suerte");
 }else{
-    document.write ("Su promedio es de "+promedio+" . ESTA DESAPROBADO, debera rendir un recuperatorio en 2 semanas,estudie mas duro.");
+    document.write ("Su promedio es de "+promedio+" . ESTA DESAPROBADO, debera rendir un recuperatorio en 2 semanas.");
 }
