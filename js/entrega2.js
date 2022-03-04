0ab646313689b257cc51b11fd0af2a473eb520a0
Ivan Adi <ivo.adi_18@hotmail.com.ar>
2022-03-03 23:33:43 -0300

    correccion

--- js/entrega2.js
+++ js/entrega2.js
@@ -12,8 +12,8 @@ function calculadora(primerNumero, segundoNumero, operacion) {
             return 0;
     }
 }
- let numero1 = parseInt(prompt("Primer numero para calcular"))
- let numero2 = parseInt(prompt("Segundo numero para calcular"))
+ let numero1 = parseInt(prompt("Ingrese el primer numero para calcular"))
+ let numero2 = parseInt(prompt("Ingrese el Segundo numero para calcular"))
  let operacion = prompt("Seleccione entre suma(+), resta (-) multiplicacion(*) o division (/)")
   alert(calculadora(numero1, numero2, operacion));
 let resultado = suma(2,3)
