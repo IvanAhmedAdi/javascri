1b5e33a5c5df685c9f56d18ae613b26835ad7d07
Ivan Adi <ivo.adi_18@hotmail.com.ar>
2022-03-03 23:28:52 -0300

    correccion

--- js/entrega2.js
+++ js/entrega2.js
@@ -1,10 +1,22 @@
-var parcial, parcial2, promedio;
-parcial = parseFloat (prompt("Ingrese nota numerica del primer examen:"));
-parcial2 = parseFloat (prompt("Y ahora,ingrese segunda calificacion obtenida en el otro parcial:"));
-promedio = (parcial+parcial2)/2;
-
-if(promedio >= 7){
-    document.write ("Su promedio es de "+promedio+" . ESTA APROBADO, Nos vemos en el final en 3 semanas,suerte");
-}else{
-    document.write ("Su promedio es de "+promedio+" . ESTA DESAPROBADO, debera rendir un recuperatorio en 2 semanas.");
+function calculadora(primerNumero, segundoNumero, operacion) {
+    switch (operacion) {
+        case "+":
+            return primerNumero + segundoNumero;
+        case "-":
+            return primerNumero - segundoNumero;
+        case "*":
+            return primerNumero * segundoNumero;
+        case "/":
+            return primerNumero / segundoNumero;
+        default:
+            return 0;
+    }
 }
+ let numero1 = parseInt(prompt("Primer numero para calcular"))
+ let numero2 = parseInt(prompt("Segundo numero para calcular"))
+ let operacion = prompt("Seleccione entre suma(+), resta (-) multiplicacion(*) o division (/)")
+  alert(calculadora(numero1, numero2, operacion));
+let resultado = suma(2,3)
+
+console.log(resultado)
+
