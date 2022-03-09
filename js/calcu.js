834f834a9a067e490d1fde6f91b38a10122d3e7b
Ivan Adi <ivo.adi_18@hotmail.com.ar>
2022-03-09 00:07:41 -0300

    preentregafinal

---
+++ js/calcu.js
@@ -0,0 +1,31 @@
+window.addEventListener('load',()=>{
+    const display = document.querySelector('.calculator-display');
+    const keypadButtons = document.getElementsByClassName('keypad-button');
+
+    const keypadButtonsArray = Array.from(keypadButtons);
+    keypadButtonsArray.forEach( (button)=>{
+        button.addEventListener('click',()=>{
+            calculadora(button, display)
+        })
+    })
+
+
+});
+
+function calculadora(button, display){
+    switch (button.innerHTML) {
+        case 'C':
+            borrar(display)
+            break;
+
+        case '=':
+            calcular(display)
+            break;
+
+        default:
+            actualizar(display, button)
+            break;    
+    }
+}
+
+funtion
