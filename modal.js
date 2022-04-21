b8b9ecde0b0d6e43759fd3c9151ecf0e59452bf6
Ivan Adi <ivo.adi_18@hotmail.com.ar>
2022-04-21 20:46:10 -0300

    pokestore

--- modal.js
+++ modal.js
@@ -1,7 +1,7 @@
-const contenedorModal = document.getElementsByClassName('modal-contenedor')[0]
-const botonAbrir = document.getElementById('boton-carrito')
-const botonCerrar = document.getElementById('carritoCerrar')
-const modalCarrito = document.getElementsByClassName('modal-carrito')[0]
+const contenedorModal = document.getElementsByClassName('modal-contenedor')[0];
+const botonAbrir = document.getElementById('boton-carrito');
+const botonCerrar = document.getElementById('carritoCerrar');
+const modalCarrito = document.getElementsByClassName('modal-carrito')[0];
 
 
 botonAbrir.addEventListener('click', ()=>{
@@ -16,6 +16,6 @@ contenedorModal.addEventListener('click', (event) =>{
 
 })
 modalCarrito.addEventListener('click', (event) => {
-    event.stopPropagation() //cuando clickeo sobre el modal se finaliza la propagacion del click a los elementos
-    //padre
+    event.stopPropagation() 
 })
+
